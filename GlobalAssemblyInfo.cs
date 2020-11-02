672e47e74868efab0e69e3ad62922dc8f9e04c64
matt v <matt_v_@users.noreply.github.com>
2020-11-01 23:41:51 -0800

    v0.5.6 Added Global Assembly Info
    Added global assembly info file (GlobalAssemblyInfo.cs), added link to all projects, configured global assembly versions for footers, disabled assembly info auto generation in each individual project.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("DogMatch v2")]
+[assembly: AssemblyCompany("Matt Vicari")]
+[assembly: AssemblyCopyright("Copyright © DogMath 2020")]
+
+[assembly: AssemblyVersion("0.5.0.6")]
+[assembly: AssemblyFileVersion("0.5.0.6")]
+[assembly: AssemblyInformationalVersion("0.5.6")]
